e16fc1b99f1c2cb2379069078c4f0c67108c200e
wikky2012 <wicliferono@gmail.com>
2020-10-30 17:55:37 +0300

    install requirements

---
+++ static/js/scripts.js
@@ -0,0 +1,44 @@
+$(document).ready(function(){
+    $('formletter').submit(function(event){
+      event.preventDefault()
+      form = $("formletter")
+  
+      $.ajax({
+        'url':'/ajax/newsletter/',
+        'type':'POST',
+        'data':form.serialize(),
+        'dataType':'json',
+        'success': function(data){
+          alert(data['success'])
+        },
+      })// END of Ajax method
+      $('#id_your_name').val('')
+      $("#id_email").val('')
+    }) // End of submit event
+  
+  }) // End of document ready function
+  
+  
+  $('#myCarousel').carousel({
+              interval: false
+          });
+  
+          //scroll slides on swipe for touch enabled devices
+  
+          $("#myCarousel").on("touchstart", function(event){
+  
+              var yClick = event.originalEvent.touches[0].pageY;
+              $(this).one("touchmove", function(event){
+  
+                  var yMove = event.originalEvent.touches[0].pageY;
+                  if( Math.floor(yClick - yMove) > 1 ){
+                      $(".carousel").carousel('next');
+                  }
+                  else if( Math.floor(yClick - yMove) < -1 ){
+                      $(".carousel").carousel('prev');
+                  }
+              });
+              $(".carousel").on("touchend", function(){
+                  $(this).off("touchmove");
+              });
+          });
--- theneighbourhoodproject/settings.py
+++ theneighbourhoodproject/settings.py
@@ -10,8 +10,16 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
+from pathlib import Path
 import os
+from decouple import config
 
+# Email configurations remember to install python-decouple
+EMAIL_USE_TLS = config('EMAIL_USE_TLS')
+EMAIL_HOST = config('EMAIL_HOST')
+EMAIL_PORT = config('EMAIL_PORT')
+EMAIL_HOST_USER = config('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -31,12 +39,17 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'neighbourhoodapp',
+    'bootstrap4',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'registration',
+    'rest_framework',
+    'rest_framework.authtoken',
 ]
 
 MIDDLEWARE = [
@@ -75,8 +88,10 @@ WSGI_APPLICATION = 'theneighbourhoodproject.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME':'neighbourhood',
+        'USER':'wikky',
+        'PASSWORD':'31406611'
     }
 }
 
@@ -105,7 +120,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Africa/Nairobi'
 
 USE_I18N = True
 
@@ -118,3 +133,15 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework.authentication.TokenAuthentication',
+    )
+}
